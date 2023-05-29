f1726d94d45c60bec10b5b495ee2a80b25220899
Kapareliotis Elias (Καπαρελιώτης Ηλίας) <helpis@tutamail.com>
2023-05-29 14:49:19 +0300

    feat(base64image): Change cache implementation by utilizing the official FileImage as a cache provider and remove unnecessary costly computations (decoding the image in every build even if it is cached).

--- lib/src/widgets/base64image.dart
+++ lib/src/widgets/base64image.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
 import 'dart:convert' show base64Decode;
 import 'dart:core';
-import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart' show getTemporaryDirectory;
 import 'package:universal_io/io.dart';
@@ -35,27 +33,6 @@ class Base64Image extends StatelessWidget {
   /// A callback that can be used to modify the image widget.
   final EditImageCallback? edit;
 
-  /// Decodes the base64 encoded [source] string into a [Uint8List] of raw image
-  /// bytes.
-  Uint8List get _imageBytes => base64Decode(source);
-
-  /// Generates a SHA-256 hash of the [source] string to use as the image ID.
-  String get _id => source.sha256asString;
-
-  /// A [CacheImageProvider] that loads the image from the [Uint8List] of raw
-  /// image bytes.
-  CacheImageProvider get _provider => CacheImageProvider(_id, _imageBytes);
-
-  /// A [CachedBase64Image] widget that displays the image.
-  Widget get _image => CachedBase64Image(provider: _provider);
-
-  /// Gets the temporary directory path and generates a path to save the image
-  /// file.
-  Future<String> get _tempDir async {
-    final String path = (await getTemporaryDirectory()).path;
-    return join(path, 'cache', 'images', _id);
-  }
-
   @override
   Widget build(final BuildContext context) => FutureBuilder<String>(
         future: Future<String>.value(source),
@@ -64,29 +41,42 @@ class Base64Image extends StatelessWidget {
           final AsyncSnapshot<String> snapshot,
         ) {
           if (!snapshot.hasData) {
-            return ConstrainedBox(
-            	constraints: constraints,
-            	child: const Row(
-            		mainAxisSize: MainAxisSize.min,
-            		children: <Widget>[
-            			Flexible(
-            				child: CircularProgressIndicator.adaptive(),
-            			),
-            		],
-            	),
-            );
+            return LoadingImageIndicator(constraints: constraints);
           }
-          final File image = File(snapshot.data!);
-          final bool exists = image.existsSync();
-          if (!exists) {
-            image
-              ..createSync(recursive: true)
-              ..writeAsBytesSync(_imageBytes);
-          }
+          final String resolvedSource = snapshot.data!;
+          final String id = resolvedSource.sha256asString;
+          return FutureBuilder<String>(
+            future: Future<String>.microtask(
+              () async {
+                final String path = (await getTemporaryDirectory()).path;
+                return join(path, 'cache', 'images', id);
+              },
+            ),
+            builder: (
+              final BuildContext context,
+              final AsyncSnapshot<String> snapshot,
+            ) {
+              if (!snapshot.hasData) {
+                return LoadingImageIndicator(constraints: constraints);
+              }
+              final File imageFile = File(snapshot.data!);
 
-          return ConstrainedBox(
-            constraints: constraints,
-            child: edit?.call(_image) ?? _image,
+              if (!imageFile.existsSync()) {
+                imageFile
+                  ..createSync(recursive: true)
+                  ..writeAsBytesSync(base64Decode(resolvedSource));
+              }
+
+              final Widget image = CachedBase64Image(
+                source: resolvedSource,
+                id: id,
+                file: imageFile,
+              );
+              return ConstrainedBox(
+                constraints: constraints,
+                child: edit?.call(image) ?? image,
+              );
+            },
           );
         },
       );
@@ -95,22 +85,57 @@ class Base64Image extends StatelessWidget {
   void debugFillProperties(final DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties
-      ..add(StringProperty('source', source))
       ..add(DiagnosticsProperty<Constraints>('constraints', constraints))
-      ..add(ObjectFlagProperty<EditImageCallback?>.has('edit', edit));
+      ..add(ObjectFlagProperty<EditImageCallback?>.has('edit', edit))
+      ..add(DiagnosticsProperty<FutureOr<String>>('source', source));
+  }
+}
+
+class LoadingImageIndicator extends StatelessWidget {
+  const LoadingImageIndicator({
+    required this.constraints,
+    super.key,
+  });
+
+  final BoxConstraints constraints;
+
+  @override
+  Widget build(final BuildContext context) => ConstrainedBox(
+        constraints: constraints,
+        child: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Flexible(
+              child: CircularProgressIndicator.adaptive(),
+            ),
+          ],
+        ),
+      );
+
+  @override
+  void debugFillProperties(final DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(
+      DiagnosticsProperty<BoxConstraints>('constraints', constraints),
+    );
   }
 }
 
 /// A callback that can be used to modify the image widget.
 typedef EditImageCallback = Widget Function(Widget image);
 
-/// A widget that displays an image loaded from a [CacheImageProvider].
-///
-/// The [provider] parameter is the [CacheImageProvider] that loads the image.
+/// A widget that displays an image loaded from a [FileImage].
 class CachedBase64Image extends StatefulWidget {
-  const CachedBase64Image({required this.provider, super.key});
+  const CachedBase64Image({
+    required this.source,
+    required this.id,
+    required this.file,
+    super.key,
+  });
 
-  final CacheImageProvider provider;
+  final String source;
+  final String id;
+  final File file;
 
   @override
   CachedBase64ImageState createState() => CachedBase64ImageState();
@@ -118,22 +143,27 @@ class CachedBase64Image extends StatefulWidget {
   @override
   void debugFillProperties(final DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
-    properties.add(
-      DiagnosticsProperty<CacheImageProvider>('provider', provider),
-    );
+    properties
+      ..add(StringProperty('source', source))
+      ..add(StringProperty('id', id))
+      ..add(DiagnosticsProperty<File>('file', file));
   }
 }
 
-/// A stateful widget that displays an image loaded from a [CacheImageProvider].
-///
-/// The [CachedBase64Image.provider] parameter is the [CacheImageProvider] that
-/// loads the image.
 class CachedBase64ImageState extends State<CachedBase64Image> {
   /// The [ImageStream] currently being listened to.
   ImageStream? _imageStream;
 
   /// The current [ImageInfo] object for the image.
   ImageInfo? _imageInfo;
+
+  late final ImageProvider _provider;
+
+  @override
+  void initState() {
+    super.initState();
+    _provider = FileImage(widget.file);
+  }
 
   /// Calls [_getImage] when the widget's dependencies change.
   @override
@@ -146,7 +176,7 @@ class CachedBase64ImageState extends State<CachedBase64Image> {
   @override
   void didUpdateWidget(final CachedBase64Image oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (widget.provider != oldWidget.provider) {
+    if (widget.id != oldWidget.id || widget.source != oldWidget.source) {
       _getImage();
     }
   }
@@ -155,7 +185,7 @@ class CachedBase64ImageState extends State<CachedBase64Image> {
   void _getImage() {
     final ImageStream? oldImageStream = _imageStream;
     final ImageConfiguration config = createLocalImageConfiguration(context);
-    _imageStream = widget.provider.resolve(config);
+    _imageStream = _provider.resolve(config);
     if (_imageStream!.key != oldImageStream?.key) {
       final ImageStreamListener listener = ImageStreamListener(_updateImage);
       oldImageStream?.removeListener(listener);
@@ -181,85 +211,8 @@ class CachedBase64ImageState extends State<CachedBase64Image> {
   }
 
   @override
-  Widget build(final BuildContext context) =>
-      RawImage(image: _imageInfo?.image, scale: _imageInfo?.scale ?? 1.0);
-}
-
-/// A [CacheImageProvider] that loads an image from a cached [Uint8List].
-///
-/// The [tag] parameter is a unique identifier for the image.
-/// The [img] parameter is the [Uint8List] of raw image bytes.
-///
-/// ```dart
-/// final CacheImageProvider provider = CacheImageProvider(
-///   'my_image',
-///   Uint8List.fromList([1, 2, 3]),
-/// );
-/// ```
-///
-/// The equality operator checks if two [CacheImageProvider]s have the same
-/// [tag].
-///
-/// The [hashCode] getter returns the hash code of the [tag].
-@immutable
-class CacheImageProvider extends ImageProvider<CacheImageProvider> {
-  const CacheImageProvider(this.tag, this.img);
-
-  /// The [tag] parameter is a unique identifier for the image.
-  final String tag;
-
-  /// The [img] parameter is the [Uint8List] of raw image bytes.
-  final Uint8List img;
-
-  /// The [loadImage] method loads the image asynchronously and returns an
-  /// [ImageStreamCompleter].
-  @override
-  ImageStreamCompleter loadImage(
-    final CacheImageProvider key,
-    final ImageDecoderCallback decode,
-  ) =>
-      MultiFrameImageStreamCompleter(
-        codec: _loadAsync(decode), // ignore: discarded_futures
-        scale: 1,
-        debugLabel: tag,
-        informationCollector: () sync* {
-          yield ErrorDescription('Tag: $tag');
-        },
+  Widget build(final BuildContext context) => RawImage(
+        image: _imageInfo?.image,
+        scale: _imageInfo?.scale ?? 1.0,
       );
-
-  /// The [_loadAsync] method loads the raw image bytes and decodes them into a
-  /// [Codec]. If the image is empty, it evicts the image from the cache and
-  /// throws an error.
-  Future<Codec> _loadAsync(final ImageDecoderCallback decode) async {
-    // the DefaultCacheManager() encapsulation, it get cache from local storage.
-    final Uint8List bytes = img;
-
-    if (bytes.lengthInBytes == 0) {
-      // The file may become available later.
-      PaintingBinding.instance.imageCache.evict(this);
-      throw StateError('$tag is empty and cannot be loaded as an image.');
-    }
-
-    return decode(await ImmutableBuffer.fromUint8List(bytes));
-  }
-
-  /// The [obtainKey] method returns a [Future] that resolves to the
-  /// [CacheImageProvider] synchronously.
-  @override
-  Future<CacheImageProvider> obtainKey(
-    final ImageConfiguration configuration,
-  ) =>
-      SynchronousFuture<CacheImageProvider>(this);
-
-  @override
-  bool operator ==(final Object other) {
-    if (other.runtimeType != runtimeType) {
-      return false;
-    }
-    final bool res = other is CacheImageProvider && other.tag == tag;
-    return res;
-  }
-
-  @override
-  int get hashCode => tag.hashCode;
 }
