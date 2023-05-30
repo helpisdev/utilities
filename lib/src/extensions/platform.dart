8df0932f381048bdd44d8bb1ff14a123e6da8eaf
Kapareliotis Elias (Καπαρελιώτης Ηλίας) <helpis@tutamail.com>
2023-05-30 17:49:23 +0300

    fix(platform): Make methods static since there isn't any cross-platform way to instantiate Platform.

--- lib/src/extensions/platform.dart
+++ lib/src/extensions/platform.dart
@@ -5,20 +5,20 @@ import 'package:universal_io/io.dart';
 extension PlatformType on Platform {
   /// Returns `true` if the platform is a desktop platform (Linux, Windows,
   /// macOS).
-  bool get isDesktop =>
+  static bool get isDesktop =>
       Platform.isLinux || Platform.isWindows || Platform.isMacOS;
 
   /// Returns `true` if the platform is a mobile platform (iOS, Android,
   /// Fuchsia).
-  bool get isMobile =>
+  static bool get isMobile =>
       Platform.isIOS || Platform.isAndroid || Platform.isFuchsia;
 
   /// Returns `true` if the platform is the web.
-  bool get isWeb => kIsWeb;
+  static bool get isWeb => kIsWeb;
 
   /// Returns `true` if the platform is a desktop platform or the web.
-  bool get isDesktopOrWeb => isDesktop || isWeb;
+  static bool get isDesktopOrWeb => isDesktop || isWeb;
 
   /// Returns `true` if the platform is a mobile platform or the web.
-  bool get isMobileOrWeb => isMobile || isWeb;
+  static bool get isMobileOrWeb => isMobile || isWeb;
 }
