4f39550bbdc0863826769d2a8edb52a4973ee8a3
Kapareliotis Elias (Καπαρελιώτης Ηλίας) <helpis@tutamail.com>
2023-05-30 16:15:05 +0300

    fix(platform): Use universal_io instead of platform to make the platform extension usable on every platform (including web).

--- lib/src/extensions/platform.dart
+++ lib/src/extensions/platform.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/foundation.dart';
-import 'package:platform/platform.dart';
+import 'package:universal_io/io.dart';
 
 /// Extends [Platform] to add platform type checks.
 extension PlatformType on Platform {
   /// Returns `true` if the platform is a desktop platform (Linux, Windows,
   /// macOS).
-  bool get isDesktop => isLinux || isWindows || isMacOS;
+  bool get isDesktop =>
+      Platform.isLinux || Platform.isWindows || Platform.isMacOS;
 
   /// Returns `true` if the platform is a mobile platform (iOS, Android,
   /// Fuchsia).
-  bool get isMobile => isIOS || isAndroid || isFuchsia;
+  bool get isMobile =>
+      Platform.isIOS || Platform.isAndroid || Platform.isFuchsia;
 
   /// Returns `true` if the platform is the web.
   bool get isWeb => kIsWeb;
