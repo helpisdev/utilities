11838721fa77ee66eb09714a11ac82e53b2941b0
Ilias Kapareliotis (Ηλίας Καπαρελιώτης) <where-at@helpis.dev>
2023-10-19 04:04:45 +0300

    feat(types): Add JSON entry type.

--- lib/src/utils/typedefs.dart
+++ lib/src/utils/typedefs.dart
@@ -3,8 +3,11 @@ import 'package:flutter/widgets.dart';
 /// A function that builds a [Widget] given a [BuildContext] and index.
 typedef PageItemBuilder = Widget Function(BuildContext, int);
 
-/// A Map representing JSON data.
+/// A [Map] representing JSON data.
 typedef JSON = Map<String, dynamic>;
+
+/// Represents a [JSON] data pair.
+typedef JSONEntry = MapEntry<String, dynamic>;
 
 /// A function that converts an [Iterable<dynamic>] into a dynamic value.
 typedef IterableConverter = dynamic Function(Iterable<dynamic> el);
